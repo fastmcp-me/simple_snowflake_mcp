fc3ca52dcc82711c09fe212454426f1a8c1d5fd4
YANN BARRAUD <yann@barraud.io>
2025-09-24 12:21:32 +0200

    v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="simple-snowflake-mcp",
-    version="0.1.0",
+    version="0.2.0",
     description="Simple Snowflake MCP Server to work behind a corporate proxy",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
